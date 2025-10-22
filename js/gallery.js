48a3c8a307015bc937688fe5eb94f730fc575529
lazoadriane5-maker <lazoadriane5@gmail.com>
2025-10-22 21:23:50 +0800

    commit

---
+++ js/gallery.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slides img');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+let index = 0;
+
+function showSlide(i) {
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[i].classList.add('active');
+}
+
+next.addEventListener('click', () => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+});
+
+prev.addEventListener('click', () => {
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index);
+});
